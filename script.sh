6507c4e6f8d385e1efd52318941d70e1b630aa69
Prateek Mishra <64471391+pra-cloud@users.noreply.github.com>
2024-04-16 12:42:37 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,22 @@ install_wsl_open() {
     npm install -g wsl-open
 }
 
+# Function to check if Docker is installed and running
+check_docker() {
+    if ! command -v docker &>/dev/null; then
+        echo "Docker is not installed. Please install Docker."
+        exit 1
+    fi
+
+    if ! docker info &>/dev/null; then
+        echo "Docker is not running. Starting Docker service..."
+        service docker start
+    fi
+}
+
+# Check if Docker is installed and running
+check_docker
+
 # Check if wsl-open command is available
 if ! command -v wsl-open &>/dev/null; then
     echo "wsl-open is not installed. Installing..."
@@ -35,5 +51,6 @@ remove_image hyperzoddevops/hyperzod-ondemand-test
 docker run -d -p 5000:5000 --name hyperzod-container hyperzoddevops/hyperzod-ondemand-test
 
 sleep 5
+
 # Open localhost:5000 in a web browser using wsl-open
 wsl-open http://localhost:5000
